3d64352e976b9695099057687cf4533b9d28348c
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-04-27 18:56:57 +0530

    Public Key Copying Feature added

--- bcpMonCLI.py
+++ bcpMonCLI.py
@@ -1,9 +1,10 @@
 import sys
+import subprocess
 import mysql.connector
 from tabulate import tabulate
 
 arguments = sys.argv
-
+  
 def main(arguments):
     if len(arguments) == 1:
         print("Error: To few arguments. Unable to execute. Use --help flag to see arguments.")
@@ -95,28 +96,38 @@ def listProjects():
     sql_query = """ 
         select DISTINCT projectName from bcpServerDetails
     """
-    output = fetchFromDatabase(sql_query)
-    for row in output:
-        print(row[0])
+    try: 
+        output = fetchFromDatabase(sql_query)
+        for row in output:
+            print(row[0])
+    except:
+        print("Error: Failed to Fetch Data from the database")
 
 def listRules():
     sql_query = """ 
         select * from bcpSyncRules
     """
-    output = fetchFromDatabase(sql_query)
-    data_as_list = [list(item) for item in output]
-    headers = ["ID", "Project Name", "Local Server Path", "BCP Server Path", "serversID", "Extensions", "Alias"]
-    print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+    try:
+        output = fetchFromDatabase(sql_query)
+        data_as_list = [list(item) for item in output]
+        headers = ["ID", "Project Name", "Local Server Path", "BCP Server Path", "serversID", "Extensions", "Alias"]
+        print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+    except:
+        print("Error: Failed to Fetch Data from the database")
 
 def listServers():
     sql_query = """ 
         select * from bcpServerDetails
     """
-    output = fetchFromDatabase(sql_query)
-    
-    data_as_list = [list(item) for item in output]
-    headers = ["Servers ID", "Project Name", "Local Server IP", "Local Server Username", "BCP Server IP", "BCP Server Username", "Alias"]
-    print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+    try:
+        output = fetchFromDatabase(sql_query)
+        
+        data_as_list = [list(item) for item in output]
+        headers = ["Servers ID", "Project Name", "Local Server IP", "Local Server Username", "BCP Server IP", "BCP Server Username", "Alias"]
+        print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+        
+    except:
+        print("Error: Failed to Fetch Data from the database")
  
 def addRules():
     database = connectToDatabase()
@@ -133,36 +144,40 @@ def addRules():
     sql_query = f""" 
         select serversID, projectName, alias from bcpServerDetails where projectName = "{projectName}"
     """
-    dbcursor.execute(sql_query)
-    output = dbcursor.fetchall()
+    try: 
+        dbcursor.execute(sql_query)
+        output = dbcursor.fetchall()
 
-    data_as_list = [list(item) for item in output]
-    headers = ["ID", "Project Name", "Alias"]
-    print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
-   
-    serversID = input("Select Servers ID from above list:\t")
-    extensions = input("Extension Name: [Optional]\t")
-    if extensions == "":
-        extensions = "Any"
-    alias = input("Alias:\t")
+        data_as_list = [list(item) for item in output]
+        headers = ["ID", "Project Name", "Alias"]
+        print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+    
+        serversID = input("Select Servers ID from above list:\t")
+        extensions = input("Extension Name: [Optional]\t")
+        if extensions == "":
+            extensions = "Any"
+        alias = input("Alias:\t")
 
-    sql_query = """INSERT INTO bcpSyncRules (projectName, localServerPath, bcpServerPath, serversID, extensions, alias) VALUES (%s, %s, %s, %s, %s, %s)"""
-    values = (projectName, localServerPath, bcpServerPath, serversID, extensions, alias)
+        sql_query = """INSERT INTO bcpSyncRules (projectName, localServerPath, bcpServerPath, serversID, extensions, alias) VALUES (%s, %s, %s, %s, %s, %s)"""
+        values = (projectName, localServerPath, bcpServerPath, serversID, extensions, alias)
 
-    dbcursor.execute(sql_query, values)
-    database.commit()
+        dbcursor.execute(sql_query, values)
+        database.commit()
 
-    sql_query = f""" 
-        select * from bcpSyncRules where projectName = "{projectName}" AND localServerPath = "{localServerPath}" AND bcpServerPath = "{bcpServerPath}" AND alias = "{alias}"
-    """
-    dbcursor.execute(sql_query)
-    output = dbcursor.fetchall()
+        sql_query = f""" 
+            select * from bcpSyncRules where projectName = "{projectName}" AND localServerPath = "{localServerPath}" AND bcpServerPath = "{bcpServerPath}" AND alias = "{alias}"
+        """
+        dbcursor.execute(sql_query)
+        output = dbcursor.fetchall()
 
-    data_as_list = [list(item) for item in output]
-    headers = ["ID", "Project Name", "Local Server Path", "BCP Server Path", "serversID", "Extensions", "Alias"]
-    print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
-    print("INFO: Rule added successfully.")
+        data_as_list = [list(item) for item in output]
+        headers = ["ID", "Project Name", "Local Server Path", "BCP Server Path", "serversID", "Extensions", "Alias"]
+        print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+        print("INFO: Rule added successfully.")
 
+    except:
+        print("Error: Adding new record is failed")
+    
 def removeRuleID():
     listRules()
     database = connectToDatabase()
@@ -172,12 +187,14 @@ def removeRuleID():
     sql_query = f""" 
         DELETE FROM bcpSyncRules WHERE ruleID = '{ruleID}';
     """
-
-    dbcursor.execute(sql_query)
-    database.commit()
-    dbcursor.close()
-    database.close()
-    print("INFO: Rule has been removed successfully.")    
+    try:
+        dbcursor.execute(sql_query)
+        database.commit()
+        dbcursor.close()
+        database.close()
+        print("INFO: Rule has been removed successfully.")
+    except:
+        print("Error: Failed to remove Data from the database")    
     
 def addNewServer():
     database = connectToDatabase()
@@ -187,11 +204,24 @@ def addNewServer():
     projectName = input("Project Name:\t")
     localServerIP = input("Local Server IP:\t")
     localUsername = input("Local Server Username:\t")
+    print(f"INFO: Copying public Key to the {localServerIP} for {localUsername}")
     
-    # Copy the Public Key to the Local Server
+    try:
+        LINUX_COMMAND = f'''ssh-copy-id {localUsername}@{localServerIP}'''
+        process = subprocess.check_output(LINUX_COMMAND, shell=True, stderr=subprocess.STDOUT)
+    except:
+        print(f"Error: Unable to copy the public to {localServerIP} for {localUsername}")
+        exit()
     
     BCPServerIP = input("BCP Server IP:\t")
     BCPUsername = input("BCP Server Username:\t")
+    
+    try:
+        LINUX_COMMAND = f'''ssh-copy-id {BCPUsername}@{BCPServerIP}'''
+        process = subprocess.check_output(LINUX_COMMAND, shell=True, stderr=subprocess.STDOUT)
+    except:
+        print(f"Error: Unable to copy the public to {BCPServerIP} for {BCPUsername}")
+        exit()
     
     # Copy the Public Key to the BCP Server
     
@@ -206,13 +236,16 @@ def addNewServer():
     sql_query = f""" 
         select * from bcpServerDetails where projectName = "{projectName}" AND localServerIP = "{localServerIP}" AND localUsername = "{localUsername}" AND BCPServerIP = "{BCPServerIP}" AND BCPUsername = "{BCPUsername}"
     """
-    dbcursor.execute(sql_query)
-    output = dbcursor.fetchall()
+    try:
+        dbcursor.execute(sql_query)
+        output = dbcursor.fetchall()
 
-    data_as_list = [list(item) for item in output]
-    headers = ["Servers ID", "Project Name", "Local Server IP", "Local Server Username", "BCP Server IP", "BCP Server Username", "Alias"]
-    print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
-    print("INFO: New Server added successfully.")
+        data_as_list = [list(item) for item in output]
+        headers = ["Servers ID", "Project Name", "Local Server IP", "Local Server Username", "BCP Server IP", "BCP Server Username", "Alias"]
+        print(tabulate(data_as_list, headers=headers, tablefmt="grid"))
+        print("INFO: New Server added successfully.")
+    except:
+        print("Error: Failed to Fetch Data from the database")
 
 def removServer():
     listServers()
@@ -244,15 +277,19 @@ def removServer():
         sql_query = f""" 
             DELETE FROM bcpServerDetails WHERE serversID = '{removeServerID}';
         """
-        dbcursor.execute(sql_query)
-   
+        try:
+            dbcursor.execute(sql_query)
+            database.commit()
+            dbcursor.close()
+            database.close()
+        except:
+            print("Error: Failed to Add Data to the database")
+            
         print("INFO: Server has been removed successfully.")
     else:
         print("INFO: Server was not removed")
  
-    database.commit()
-    dbcursor.close()
-    database.close()
+    
         
     
 if __name__ == "__main__":
