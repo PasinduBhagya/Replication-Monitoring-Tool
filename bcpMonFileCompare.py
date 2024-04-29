320a3f7bdf33915eb6126d23bc1ac15c2543633b
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-04-29 10:03:34 +0000

    Caching mechanisum added

--- bcpMonFileCompare.py
+++ bcpMonFileCompare.py
@@ -105,60 +105,73 @@ def getRuleID():
     
     SYNC_STATUS = "Success"
     
+    checkedRulesList = []
+
+    with open(".cache/checkedRules", 'r') as checkedRules:
+        for checkedRule in checkedRules:
+            checkedRulesList.append(int(checkedRule.strip()))
+    
     sql_query = """ 
         select * from bcpSyncRules
     """
     rulesInfo = fetchFromDatabase(sql_query)
     
     for ruleInfo in rulesInfo:
+        print(ruleInfo)
         print("-"*90)
         print("INFO: Starting rule ID - " + str(ruleInfo[0]) + " " + str(ruleInfo[-1]))
-        
-        projectName = ruleInfo[1]
-        localServerPath = ruleInfo[2]
-        bcpServerPath = ruleInfo[3]
-        serversID = ruleInfo[4]
-        extensions = ruleInfo[5]
-        alias = ruleInfo[6]
-        
-        print("INFO: Servers ID - " + str(ruleInfo[4]))
-        
-        sql_query = f""" 
-            select * from bcpServerDetails where serversID = "{serversID}"
-        """
-        serversInfo = fetchFromDatabase(sql_query)
-        
-        localServerIP = serversInfo[0][2]
-        localUsername = serversInfo[0][3]
-        BCPServerIP = serversInfo[0][4]
-        BCPUsername = serversInfo[0][5]
-        
-        LOCAL_MD5SUM_HASH = getLocalServerMD5Sum(localServerPath, extensions, localServerIP, localUsername, projectName)
-        BCP_MD5SUM_HASH = getBCPServerMD5Sum(bcpServerPath, extensions, BCPServerIP, BCPUsername, projectName)
-        try:
-            for key, value in LOCAL_MD5SUM_HASH.items():
-                if key in BCP_MD5SUM_HASH:
-                    if value == BCP_MD5SUM_HASH[key]:
-                        print(f"INFO: File {key} is same on the both servers.")
-                    else:
-                        print(f"Warning: File {key} is not same on the both servers.")
-                        SYNC_STATUS = "Failed"        
-                else:
-                    print(f"Warning: File {key} was not found in BCP Enviorment.")
-                    SYNC_STATUS = "Failed"
+
+        if int(ruleInfo[0]) in checkedRulesList:
+            print(f"INFO: Skipping rule ID - {ruleInfo[0]}.")
+
+        else:
+            projectName = ruleInfo[1]
+            localServerPath = ruleInfo[2]
+            bcpServerPath = ruleInfo[3]
+            serversID = ruleInfo[4]
+            extensions = ruleInfo[5]
+            alias = ruleInfo[6]
             
-            if not os.path.exists("DATA/" + DATE_FOLDER):
-                try:
-                    os.makedirs("DATA/" + DATE_FOLDER)
-                    print(f"INFO: {DATE_FOLDER} folder has been created successfully.")
-                except:
-                    print("Error: Unable to create the DATA/" + DATE_FOLDER)
+            print("INFO: Servers ID - " + str(ruleInfo[4]))
+            
+            sql_query = f""" 
+                select * from bcpServerDetails where serversID = "{serversID}"
+            """
+            serversInfo = fetchFromDatabase(sql_query)
+            
+            localServerIP = serversInfo[0][2]
+            localUsername = serversInfo[0][3]
+            BCPServerIP = serversInfo[0][4]
+            BCPUsername = serversInfo[0][5]
+            
+            LOCAL_MD5SUM_HASH = getLocalServerMD5Sum(localServerPath, extensions, localServerIP, localUsername, projectName)
+            BCP_MD5SUM_HASH = getBCPServerMD5Sum(bcpServerPath, extensions, BCPServerIP, BCPUsername, projectName)
+            try:
+                for key, value in LOCAL_MD5SUM_HASH.items():
+                    if key in BCP_MD5SUM_HASH:
+                        if value == BCP_MD5SUM_HASH[key]:
+                            print(f"INFO: File {key} is same on the both servers.")
+                        else:
+                            print(f"Warning: File {key} is not same on the both servers.")
+                            SYNC_STATUS = "Failed"        
+                    else:
+                        print(f"Warning: File {key} was not found in BCP Enviorment.")
+                        SYNC_STATUS = "Failed"
                 
-            with open("DATA/" + DATE_FOLDER + "/" +  projectName, 'a') as projectFile:
-                projectFile.write(f"{alias},{SYNC_STATUS}\n")
-        except:
-            print("Error: The server did not return data for given extensions.") 
-        print("-"*90)    
+                if not os.path.exists("DATA/" + DATE_FOLDER):
+                    try:
+                        os.makedirs("DATA/" + DATE_FOLDER)
+                        print(f"INFO: {DATE_FOLDER} folder has been created successfully.")
+                    except:
+                        print("Error: Unable to create the DATA/" + DATE_FOLDER)
+                    
+                with open("DATA/" + DATE_FOLDER + "/" +  projectName, 'a') as projectFile:
+                    projectFile.write(f"{alias},{SYNC_STATUS}\n")
+                with open(".cache/checkedRules", 'a') as checkedRules:
+                    checkedRules.write("\n"+str(ruleInfo[0]))
+            except:
+                print("Error: The server did not return data for given extensions.") 
+            print("-"*90)    
 
         
 def main():
